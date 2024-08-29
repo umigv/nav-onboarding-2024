f6078658f0ca244ec776143b8dacc16133d1eea5
John Rose <johnrose@umich.edu>
2024-08-29 19:00:37 -0400

    Added jso json library

--- order_publisher/src/main.cpp
+++ order_publisher/src/main.cpp
@@ -1,6 +1,9 @@
 #include "order_publisher.h"
 
-int main()
+int main(int argc, char **argv)
 {
-    return 0;
+	rclcpp::init(argc, argv);
+	rclcpp::spin(std::make_shared<OrderPublisher>());
+	rclcpp::shutdown();
+	return 0;
 }
--- order_publisher/src/order_publisher.cpp
+++ order_publisher/src/order_publisher.cpp
@@ -1,1 +1,32 @@
 #include "order_publisher.h"
+
+#include <chrono>
+#include <memory>
+#include <iostream>
+
+using namespace std::chrono_literals;
+
+OrderPublisher::OrderPublisher()
+    : Node("order_publisher_node"),
+    _order_count(0)
+{
+    _order_publisher = create_publisher<order_publisher::msg::Order>("orders", 10);
+    _order_timer = create_wall_timer(1s, 
+    std::bind(&OrderPublisher::publish_order, 
+    this));
+}
+
+void OrderPublisher::publish_order()
+{
+    std::cout << "Hello from publish_order\n";
+
+    order_publisher::msg::Order order_message;
+    order_message.order_id = get_order_id();
+}
+
+int OrderPublisher::get_order_id()
+{
+    int order_id = _order_count;
+    _order_count++;
+    return order_id;
+}
