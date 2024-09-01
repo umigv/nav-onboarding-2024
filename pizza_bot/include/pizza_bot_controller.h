d822ceb0bfd1c2391a727360764085349727efec
John Rose <johnrose@umich.edu>
2024-08-31 22:41:40 -0400

    Wrote navigate_to_coord client

--- pizza_bot/include/pizza_bot_controller.h
+++ pizza_bot/include/pizza_bot_controller.h
@@ -3,6 +3,7 @@
 
 #include "rclcpp/rclcpp.hpp"
 #include "pizza_bot_interfaces/msg/order.hpp"
+#include "pizza_bot_interfaces/srv/navigate_to_coord.hpp"
 
 class PizzaBotController : public rclcpp::Node
 {
@@ -14,8 +15,17 @@ private:
 
     void order_callback(pizza_bot_interfaces::msg::Order::SharedPtr order);
 
+    // Calls the navigate_to_coord service with the given goal asynchronously
+    // The given response_callback will be called with the response once the service finishes
+    void call_navigate_service(const pizza_bot_interfaces::msg::Coord &goal,
+        std::function<void(rclcpp::Client<pizza_bot_interfaces::srv::NavigateToCoord>::SharedFuture)> response_callback);
+
+    void pizza_place_navigation_callback(rclcpp::Client<pizza_bot_interfaces::srv::NavigateToCoord>::SharedFuture future,
+        const pizza_bot_interfaces::msg::Coord &goal);
+
     rclcpp::Subscription<pizza_bot_interfaces::msg::Order>::SharedPtr _order_subscriber;
     rclcpp::Publisher<pizza_bot_interfaces::msg::Order>::SharedPtr _received_order_publisher;
+    rclcpp::Client<pizza_bot_interfaces::srv::NavigateToCoord>::SharedPtr _navigate_client;
 };
 
 #endif
--- pizza_bot/src/pizza_bot_controller.cpp
+++ pizza_bot/src/pizza_bot_controller.cpp
@@ -1,5 +1,12 @@
 #include "pizza_bot_controller.h"
 
+#include <chrono>
+#include <memory>
+#include <functional>
+
+using namespace std::chrono_literals;
+using NavigateToCoordSharedFuture = rclcpp::Client<pizza_bot_interfaces::srv::NavigateToCoord>::SharedFuture;
+
 PizzaBotController::PizzaBotController()
     : Node("pizza_bot_controller_node")
 {
@@ -10,9 +17,50 @@ PizzaBotController::PizzaBotController()
             std::placeholders::_1));
 
     _received_order_publisher = create_publisher<pizza_bot_interfaces::msg::Order>("received_orders", 10);
+    _navigate_client = create_client<pizza_bot_interfaces::srv::NavigateToCoord>("navigate_to_coord");
 }
 
 void PizzaBotController::order_callback(pizza_bot_interfaces::msg::Order::SharedPtr order)
 {
     _received_order_publisher->publish(*order);
+    call_navigate_service(order->pizza_place_coord,
+        std::bind(&PizzaBotController::pizza_place_navigation_callback,
+            this,
+            std::placeholders::_1,
+            order->pizza_place_coord));
+}
+
+void PizzaBotController::call_navigate_service(const pizza_bot_interfaces::msg::Coord &goal,
+    std::function<void(NavigateToCoordSharedFuture)> response_callback)
+{
+    while (!_navigate_client->wait_for_service(1s)) 
+	{
+	    RCLCPP_INFO(get_logger(), 
+            "navigate_to_coord service not available, waiting again...");
+	}
+
+    auto goal_request = std::make_shared<pizza_bot_interfaces::srv::NavigateToCoord::Request>();
+    goal_request->goal = goal;
+
+    auto result = _navigate_client->async_send_request(goal_request, response_callback);
+}
+
+void PizzaBotController::pizza_place_navigation_callback(NavigateToCoordSharedFuture future,
+    const pizza_bot_interfaces::msg::Coord &goal)
+{
+    bool navigation_succeeded = future.get()->success;
+    if (navigation_succeeded)
+    {
+        RCLCPP_INFO(get_logger(),
+            "Successfuly navigated to (%ld, %ld)",
+            goal.x,
+            goal.y);
+    }
+    else
+    {
+        RCLCPP_INFO(get_logger(),
+            "Failed to navigate to (%ld, %ld)",
+            goal.x,
+            goal.y);
+    }
 }
