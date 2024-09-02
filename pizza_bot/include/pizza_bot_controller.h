ebbd8032fabc928148f69604260c39fe81190cba
John Rose <johnrose@umich.edu>
2024-09-02 15:48:56 -0400

    Wrote make_pizza action client

--- pizza_bot/include/pizza_bot_controller.h
+++ pizza_bot/include/pizza_bot_controller.h
@@ -2,8 +2,10 @@
 #define PIZZA_BOT_CONTROLLER_H
 
 #include "rclcpp/rclcpp.hpp"
+#include "rclcpp_action/rclcpp_action.hpp"
 #include "pizza_bot_interfaces/msg/order.hpp"
 #include "pizza_bot_interfaces/srv/navigate_to_coord.hpp"
+#include "pizza_bot_interfaces/action/make_pizza.hpp"
 
 class PizzaBotController : public rclcpp::Node
 {
@@ -21,11 +23,22 @@ private:
         std::function<void(rclcpp::Client<pizza_bot_interfaces::srv::NavigateToCoord>::SharedFuture)> response_callback);
 
     void pizza_place_navigation_callback(rclcpp::Client<pizza_bot_interfaces::srv::NavigateToCoord>::SharedFuture future,
-        const pizza_bot_interfaces::msg::Coord &goal);
+        const pizza_bot_interfaces::msg::Order::SharedPtr order);
+
+    void send_pizza_order(const pizza_bot_interfaces::msg::Order::SharedPtr order);
+
+    void goal_response_callback(std::shared_ptr<rclcpp_action::ClientGoalHandle<pizza_bot_interfaces::action::MakePizza>> goal_handle);
+
+    void feedback_callback(std::shared_ptr<rclcpp_action::ClientGoalHandle<pizza_bot_interfaces::action::MakePizza>> goal_handle,
+        const std::shared_ptr<const pizza_bot_interfaces::action::MakePizza::Feedback> feedback);
+
+    void result_callback(const rclcpp_action::ClientGoalHandle<pizza_bot_interfaces::action::MakePizza>::WrappedResult &result,
+        const pizza_bot_interfaces::msg::Order::SharedPtr order);
 
     rclcpp::Subscription<pizza_bot_interfaces::msg::Order>::SharedPtr _order_subscriber;
     rclcpp::Publisher<pizza_bot_interfaces::msg::Order>::SharedPtr _received_order_publisher;
     rclcpp::Client<pizza_bot_interfaces::srv::NavigateToCoord>::SharedPtr _navigate_client;
+    rclcpp_action::Client<pizza_bot_interfaces::action::MakePizza>::SharedPtr _make_pizza_client;
 };
 
 #endif
--- pizza_bot/src/pizza_bot_controller.cpp
+++ pizza_bot/src/pizza_bot_controller.cpp
@@ -5,7 +5,10 @@
 #include <functional>
 
 using namespace std::chrono_literals;
+using namespace std::placeholders;
 using NavigateToCoordSharedFuture = rclcpp::Client<pizza_bot_interfaces::srv::NavigateToCoord>::SharedFuture;
+using MakePizza = pizza_bot_interfaces::action::MakePizza;
+using MakePizzaGoalHandle = rclcpp_action::ClientGoalHandle<MakePizza>;
 
 PizzaBotController::PizzaBotController()
     : Node("pizza_bot_controller_node")
@@ -14,10 +17,11 @@ PizzaBotController::PizzaBotController()
         10, 
         std::bind(&PizzaBotController::order_callback, 
             this, 
-            std::placeholders::_1));
+            _1));
 
     _received_order_publisher = create_publisher<pizza_bot_interfaces::msg::Order>("received_orders", 10);
     _navigate_client = create_client<pizza_bot_interfaces::srv::NavigateToCoord>("navigate_to_coord");
+    _make_pizza_client = rclcpp_action::create_client<MakePizza>(this, "make_pizza");
 }
 
 void PizzaBotController::order_callback(pizza_bot_interfaces::msg::Order::SharedPtr order)
@@ -28,8 +32,8 @@ void PizzaBotController::order_callback(pizza_bot_interfaces::msg::Order::Shared
     call_navigate_service(order->pizza_place_coord,
         std::bind(&PizzaBotController::pizza_place_navigation_callback,
             this,
-            std::placeholders::_1,
-            order->pizza_place_coord));
+            _1,
+            order));
 }
 
 void PizzaBotController::call_navigate_service(const pizza_bot_interfaces::msg::Coord &goal,
@@ -48,21 +52,120 @@ void PizzaBotController::call_navigate_service(const pizza_bot_interfaces::msg::
 }
 
 void PizzaBotController::pizza_place_navigation_callback(NavigateToCoordSharedFuture future,
-    const pizza_bot_interfaces::msg::Coord &goal)
+    const pizza_bot_interfaces::msg::Order::SharedPtr order)
 {
     bool navigation_succeeded = future.get()->success;
-    if (navigation_succeeded)
+    pizza_bot_interfaces::msg::Coord pizza_place_coord = order->pizza_place_coord;
+    std::string pizza_place_name = order->pizza_place;
+    if (!navigation_succeeded)
     {
         RCLCPP_INFO(get_logger(),
-            "Successfuly navigated to (%ld, %ld)",
-            goal.x,
-            goal.y);
+            "Failed to navigate to %s at (%ld, %ld)",
+            pizza_place_name.c_str(),
+            pizza_place_coord.x,
+            pizza_place_coord.y);
+        return;
     }
-    else
+
+    RCLCPP_INFO(get_logger(),
+        "Successfuly navigated to %s at (%ld, %ld)",
+        pizza_place_name.c_str(),
+        pizza_place_coord.x,
+        pizza_place_coord.y);
+
+    send_pizza_order(order);
+}
+
+void PizzaBotController::send_pizza_order(const pizza_bot_interfaces::msg::Order::SharedPtr order)
+{
+    while (!_make_pizza_client->wait_for_action_server(1s))
+    {
+        RCLCPP_INFO(get_logger(), 
+            "make_pizza action not available, waiting again...");
+    }
+
+    MakePizza::Goal pizza_order;
+    pizza_order.pizza_type = order->pizza_type;
+
+    rclcpp_action::Client<MakePizza>::SendGoalOptions options;
+    options.goal_response_callback = std::bind(&PizzaBotController::goal_response_callback,
+        this, 
+        _1);
+    options.feedback_callback = std::bind(&PizzaBotController::feedback_callback,
+        this, 
+        _1,
+        _2);
+    options.result_callback = std::bind(&PizzaBotController::result_callback,
+        this, 
+        _1,
+        order);
+
+    _make_pizza_client->async_send_goal(pizza_order, options);
+}
+
+void PizzaBotController::goal_response_callback(std::shared_ptr<MakePizzaGoalHandle> goal_handle)
+{
+    if (!goal_handle) 
+    {
+        RCLCPP_ERROR(get_logger(), 
+            "Order was rejected by restaurant");
+    } 
+    else 
+    {
+        RCLCPP_INFO(get_logger(), 
+            "Order accepted by restaurant, waiting for pizza");
+    }
+}
+
+void PizzaBotController::feedback_callback(std::shared_ptr<MakePizzaGoalHandle> goal_handle,
+    const std::shared_ptr<const MakePizza::Feedback> feedback)
+{
+    (void)goal_handle; // Prevent unused parameter warning
+    RCLCPP_INFO(get_logger(),
+        "Feedback from restaurant: %s",
+        feedback->pizza_status.c_str());
+}
+
+void PizzaBotController::result_callback(const MakePizzaGoalHandle::WrappedResult &result,
+    const pizza_bot_interfaces::msg::Order::SharedPtr order)
+{
+    switch (result.code)
+    {
+    case rclcpp_action::ResultCode::SUCCEEDED:
+        break;
+
+    case rclcpp_action::ResultCode::ABORTED:
+        RCLCPP_ERROR(get_logger(),
+            "Order was aborted");
+        return;
+
+    case rclcpp_action::ResultCode::CANCELED:
+        RCLCPP_ERROR(get_logger(),
+            "Order was canceled");
+        return;
+
+    default:
+        RCLCPP_ERROR(get_logger(),
+            "Unknown result code");
+        break;
+    }
+
+    std::string pizza_type = order->pizza_type;
+    std::string pizza_place = order->pizza_place;
+
+    if (!result.result->pizza_finished)
     {
         RCLCPP_INFO(get_logger(),
-            "Failed to navigate to (%ld, %ld)",
-            goal.x,
-            goal.y);
+            "%s failed to complete the order for a %s pizza",
+            pizza_place.c_str(),
+            pizza_type.c_str());
+        return;
     }
+
+    RCLCPP_INFO(get_logger(),
+        "%s completed the order for a %s pizza",
+        pizza_place.c_str(),
+        pizza_type.c_str());
+
+    
 }
